2de14b1f9de550cdc79bdb7e311c0296b9e47fef
zhangxinyu5663 <1976871412@qq.com>
2019-03-13 21:20:57 +0800

    Rectangle v0.4

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,16 +26,41 @@ module.exports = function (grunt) {
         run: true,
         reporter: 'Spec'                          
       }     
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false                            
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'                            
+      }
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'               
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/rectangle.js': 'rectangle.js',
+          'dist/calc.js': 'calc.js'                                     
+        }               
+      }             
     }        
   });
 
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
-  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-mocha');  
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.registerTask('lint', ['htmlhint','csslint','eslint']);
-  grunt.registerTask('unitTest', ['mocha']);
+  grunt.registerTask('unitTest', ['mocha'])
+  grunt.resterTask('minify', ['htmlmin', 'cssmin', 'uglify']);
 };
 
 
