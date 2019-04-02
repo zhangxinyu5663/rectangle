db438fd492be377db8e596c15153036dac535ec7
zhangxinyu5663 <1976871412@qq.com>
2019-04-02 15:50:14 +0800

    h5校验

--- rectangle.js
+++ rectangle.js
@@ -5,48 +5,14 @@ $(function(){
       $btnCal=$('#calculate'),
       $perimeter=$('#perimeter'),
       $area=$('#area'),
-      $widthValidation=$('#width-validation'),
-      $heightValidation=$('#height-validation');
+      $form=$('form');
 
-  $width.focusout(function(){
-    //var w=$width.val();
-    var result=valid($width.val());
-    if(!result.isOK){
-      $widthValidation.html('宽度'+result.reason);
-      $width.select();
-      return;
-    }else{
-      $widthValidation.html('');
-    }
-  });
-
-  $height.focusout(function(){
-    var result=valid($height.val());
-    if(!result.isOK){
-      $heightValidation.html('高度'+result.reason);
-      $height.select();
-      return;
-    }else{
-      $heightValidation.html('');
-    }
-  });
-
-  $btnCal.click(function(){
+ 
+  $form.submit(function(e){
+    e.preventDefault();  //阻止表单提交时刷新
     var w=$width.val(),
         h=$height.val();
-    
-    var validWidth=valid(w),
-        validHeight=valid(h);
-    if(!validWidth.isOK){
-      $widthValidation.html('宽度'+validWidth.reason);
-      $width.select();
-    }
-
-    if(!validHeight.isOK){
-      $heightValidation.html('高度'+validHeight.reason);
-      $height.select();
-    }
-
+   
     var r=new Rectangle(w,h);
 
     $perimeter.val(r.perimeter());
